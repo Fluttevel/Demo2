03f883d21dbe91809868006778da41b0cd155362
Fluttevel <fluttevel@ya.ru>
2021-12-27 17:19:07 +0200

    Added tabulation

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  access_key = "AKIARTC7X46XGGRF7OPE"
-  secret_key = "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"
-  region = "eu-central-1"
+  access_key  = "AKIARTC7X46XGGRF7OPE"
+  secret_key  = "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"
+  region      = "eu-central-1"
 }
 
 resource "aws_instance" "instance" {
-  ami = "ami-042ad9eec03638628"
+  ami           = "ami-042ad9eec03638628"
   instance_type = "t2.micro"
 
   tags = {
