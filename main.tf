67b30056b14753ccdded751f84d2cafcde63c336
Fluttevel <fluttevel@ya.ru>
2021-12-27 16:58:57 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  access_key = "AKIARTC7X46XGGRF7OPE"
+  secret_key = "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"
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "instance" {
+  ami = "ami-042ad9eec03638628"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-instance"
+  }
+}
