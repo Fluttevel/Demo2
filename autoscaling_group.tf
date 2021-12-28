acbc59809f7a1abdae1e54e472c157b3ea723e2a
Fluttevel <fluttevel@ya.ru>
2021-12-28 19:47:22 +0200

    Init ASG

--- autoscaling_group.tf
+++ autoscaling_group.tf
@@ -1,11 +1,14 @@
 resource "aws_autoscaling_group" "example" {
   launch_configuration  = aws_launch_configuration.example.name
-  max_size              = 2
-  min_size              = 10
+  vpc_zone_identifier   = data.aws_subnet_ids.default.ids
+  target_group_arns     = [aws_alb_target_group.asg.arn]
+  health_check_type     = "ELB"
+  min_size              = 2
+  max_size              = 10
 
-  tag = {
-    key = "Name"
-    value = "terraform-asg-example"
+  tag {
+    key                 = "Name"
     propagate_at_launch = true
+    value               = "terraform-asg-example"
   }
 }
